f57297fbffecf550a817a88b6c1c6649767b8dcf
rjbhullar <rajdeep.singh@83incs.com>
2021-04-25 12:33:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useReducer } from 'react';
+// import MasonryComp from './Masnory';
+import axios from 'axios';
+
+let API_URL = "https://api.unsplash.com/photos/?client_id=QnoShfzoGvJZjOi029m4sxFmAcorM5flRkj7WtU5Ymo&per_page=20&page="
+const stateReducer = (state, action) => ({
+  ...state,
+  ...(typeof (action) == "function" ? action(state) : action)
+});
 
 function App() {
+  let [state, setState] = useReducer(stateReducer, {
+    activePageNumber: 1,
+    images: [],
+    requestState: "loading",
+  });
+
+  useEffect(() => {
+    let url = API_URL + state.activePageNumber;
+    axios.get(url)
+      .then(res => {
+        console.log('res: ', res);
+        setState(state => ({
+          images: [...state.images, ...res.data],
+          requestState: "loaded",
+        }));
+      })
+      .catch({ requestState: "error" })
+  }, [state.activePageNumber]);
+
+  let { images, requestState } = state;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      {requestState === "loading" && "loader"}
+      {/*<MasonryComp list={images} />*/}
+      {requestState === "loading" && "loading..."}
     </div>
   );
 }
